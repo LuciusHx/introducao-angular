5a7defe09db6f2477dcf6d31b1babb6ca92270e9
Lucius Hebert <luciushebert13@gmail.com>
2024-11-28 15:05:55 -0300

    estudo sobre router, css e share de dados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+
+//path é o que vai ficar na URL, dps eu coloco o nome do component que fica na pasta pages
+const routes: Routes = [
+  {path: '', component: HomeComponent}
+//{ path: 'about', component: AboutComponent }, // Página "Sobre"
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/cards/cards.component.ts
+++ src/app/shared/cards/cards.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 @Component({
   selector: 'app-cards',
   standalone: false,
-  
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
+
 export class CardsComponent {
   @Input() variant: 'primary' | 'secondary' = 'primary'
+  
 }
