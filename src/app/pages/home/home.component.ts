b310a5ab015036a983b261d4b76d10e0db1384c4
Lucius Hebert <luciushebert13@gmail.com>
2024-12-10 05:57:03 -0300

    Section Depoiments Corrigida

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,5 +9,19 @@ import { Component, Input } from '@angular/core';
 })
 export class HomeComponent {
   //to puxando 
-  @Input() compartilhando: string = 'Compartilhando Pai'
+  @Input() compartilhando: string = 'Compartilhando Pai';
+  cartoes = [
+    {
+      foto:'assets/imagens/avatar3.png', 
+      depoimento:"A agência é boa de verdade! Eu aconselho pegar na black friday. CONSEGUI, CONSEGUI, CONSEGUI", 
+      autoria:"Fulana de tal"},
+    {
+      foto:'assets/imagens/avatar3.png', 
+      depoimento:"Cuida rapaziada, é bom! Eu aconselho pegar na black friday. CONSEGUI, CONSEGUI, CONSEGUI", 
+      autoria:"Luis Nathan"},
+    {
+      foto:'assets/imagens/avatar3.png', 
+      depoimento:"Recomendo fortemente a agência de viagens Jornada! Eu aconselho pegar na black friday.", 
+      autoria:"Lucius Hebert"},
+    ];
 }
--- src/app/shared/card-depoimento/card-depoimento.component.ts
+++ src/app/shared/card-depoimento/card-depoimento.component.ts
@@ -1,37 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-depoimento',
   standalone: false,
-  
   templateUrl: './card-depoimento.component.html',
   styleUrl: './card-depoimento.component.css'
 })
 export class CardDepoimentoComponent {
-  cartoes = [
-    {
-      foto:'assets/imagens/avatar3.png', 
-      depoimento:"A agência é boa de verdade! Eu aconselho pegar na black friday.", 
-      autoria:"Fulana de tal"},
-    {
-      foto:'assets/imagens/avatar3.png', 
-      depoimento:"Cuida rapaziada, é bom! Eu aconselho pegar na black friday.", 
-      autoria:"siclano de tal"},
-    {
-      foto:'assets/imagens/avatar3.png', 
-      depoimento:"Recomendo fortemente a agência de viagens Jornada! Eu aconselho pegar na black friday.", 
-      autoria:"Lucius Hebert"},
-  ];
-//   depoimento: string = `
-//   Recomendo fortemente a agência de viagens Jornada.
-//   Eles oferecem um serviço personalizado e de alta qualidade
-//   que excedeu minhas expectativas em minha última viagem.
-// `
-  autoria: string = 'Mariana Faustino'
+  @Input() foto: string = '' 
+  @Input() depoimento: string = '' 
+  @Input() autoria: string = '' 
 
   color: string = '#000000'
   bgColor: string = '#FFFFFF'
-
   show: boolean = false
   showBG(){
     this.show = !this.show //toggle
@@ -43,6 +24,4 @@ export class CardDepoimentoComponent {
       this.bgColor = 'red'; 
     }
   }
-
-
 }
